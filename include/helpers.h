5198525116acc6589ac960a8786c3fe8e6aac2a7
amavr <albert.mavrin@gmail.com>
2023-11-24 00:29:41 +0300

    Цифровой термометр

--- include/helpers.h
+++ include/helpers.h
@@ -1,14 +1,14 @@
 uint8_t readHystBeg(uint8_t hyst){
     int val = analogRead(A1);
-    Serial.println(val);
+    // Serial.println(val);
     return map(val, 0, 1023, 10, 40 - hyst);
 }
 
-void drawReleysAndTemp(uint8_t active, float temp)
+void drawReleysAndTemp(uint8_t activeReley, float temp)
 {
     uint8_t top = 8;
-    oled.rect(4, top, 36, top + 7, active == 0 ? OLED_FILL : OLED_STROKE);
-    oled.rect(92, top, 124, top + 7, active == 1 ? OLED_FILL : OLED_STROKE);
+    oled.rect(4, top, 36, top + 7, activeReley == 0 ? OLED_FILL : OLED_STROKE);
+    oled.rect(92, top, 124, top + 7, activeReley == 1 ? OLED_FILL : OLED_STROKE);
     oled.setScale(1);
     oled.setCursorXY(50, top + 1); // курсор в (пиксель X, пиксель Y)
     oled.print(temp, 1);
@@ -39,6 +39,7 @@ void drawMode(uint8_t beg, uint8_t hyst)
 }
 
 void drawTemp(float val){
+    Serial.println(val);
     uint8_t t = constrain(round(val), 10, 40);
     uint8_t x = map(t, 10, 40, 4, 124);
     oled.circle(x - 1, 33, 3, OLED_FILL);
--- src/main.cpp
+++ src/main.cpp
@@ -9,50 +9,115 @@ GyverNTC therm(0, 10000, 3200); // пин, сопротивление при 25 
 
 #include <GyverOLED.h>
 GyverOLED<SSD1306_128x64> oled;
+// GyverOLED<SSD1306_128x64, OLED_NO_BUFFER> oled;
 
+#define TEMP_PIN 3
+#define RELAY0_PIN 2
+#define RELAY1_PIN 4
+
+#include <microDS18B20.h>
 #include "helpers.h"
 
-
 bool isActive = false;
+bool isActiveBak = false;
+
 uint8_t activeRelay = 0;
+uint8_t activeRelayBak = 3;
+
 uint16_t relayTimes = 0;
-uint16_t maxTimes = 30;
+uint16_t maxTimes = 180;
 
 uint8_t hyst_beg = 20;
-uint8_t hyst = 6;
+uint8_t hyst = 3;
 
+MicroDS18B20<TEMP_PIN> sensor;
 
 void setup()
 {
     Serial.begin(115200);
     pinMode(A1, INPUT);
+    pinMode(RELAY0_PIN, OUTPUT);
+    pinMode(RELAY1_PIN, OUTPUT);
     oled.init(); // инициализация
 }
 
 void loop()
 {
-    float t = therm.getTempAverage();
+    sensor.requestTemp();
+    delay(1000);
+    if (sensor.readTemp())
+    {
+        float t = sensor.getTemp();
+        Serial.println(t);
 
-    hyst_beg = readHystBeg(hyst);
+        hyst_beg = readHystBeg(hyst);
 
-    oled.clear();
+        isActiveBak = isActive;
+        // температура выше чем заданный режим
+        if (t > hyst_beg + hyst)
+        {
+            isActive = false;
+        }
+        // температура меньше заданного режима
+        else if (t < hyst_beg)
+        {
+            isActive = true;
+        }
 
-    relayTimes++;
-    if(relayTimes >= maxTimes){
-        relayTimes = 0;
-        activeRelay = activeRelay == 0 ? 1 : 0;
+        relayTimes++;
+        if (relayTimes >= maxTimes)
+        {
+            relayTimes = 0;
+            activeRelayBak = activeRelay;
+            activeRelay = activeRelay == 0 ? 1 : 0;
+        }
+
+        if (isActive)
+        {
+            if (activeRelay == 0)
+            {
+                if (activeRelayBak != activeRelay)
+                {
+                    digitalWrite(RELAY0_PIN, HIGH);
+                    digitalWrite(RELAY1_PIN, LOW);
+                    activeRelayBak = activeRelay;
+                }
+            }
+            else
+            {
+                if (activeRelayBak != activeRelay)
+                {
+                    digitalWrite(RELAY0_PIN, LOW);
+                    digitalWrite(RELAY1_PIN, HIGH);
+                    activeRelayBak = activeRelay;
+                }
+            }
+        }
+        else
+        {
+            if (isActiveBak != isActive)
+            {
+                digitalWrite(RELAY0_PIN, LOW);
+                digitalWrite(RELAY1_PIN, LOW);
+                isActiveBak = isActive;
+            }
+        }
+
+        oled.clear();
+        drawReleysAndTemp(isActive ? activeRelay : 2, t);
+        drawScale();
+        drawMode(hyst_beg, hyst);
+        drawTemp(t);
+        oled.update();
+    }
+    else
+    {
+        Serial.println("error");
     }
 
-    drawReleysAndTemp(activeRelay, t);
-    drawScale();
-    drawMode(hyst_beg, hyst);
-    drawTemp(t);
+    // float t = therm.getTempAverage();
+    // float t = 20.0;
+    // hyst_beg = readHystBeg(hyst);
 
-    oled.update();
-
-    // Serial.print("Temperature ");
-    // Serial.print(t);
-    // Serial.println(" *C");
-    delay(300);
+    // delay(300);
 }
-
